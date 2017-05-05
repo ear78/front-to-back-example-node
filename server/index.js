6ba7a0e0313f0ecf9ae587fefa110aaf3692569d
elliot richardson <ear78@aol.com>
2017-05-05 14:29:48 -0500

    updated crud setup

--- server/index.js
+++ server/index.js
@@ -7,8 +7,35 @@ var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/test', function(req,res,next){
-    res.status(200).send('yay');
+
+// GET ENDPOINTS //
+
+//get all products
+app.get('/products', function(req, res, next){
+
+})
+
+// find a particular product
+app.get('/products/:id', function(req, res, next){
+
+})
+
+// POST ENDPOINTS //
+// create
+app.post('/products', function(req, res, next){
+
+})
+
+// PUT ENDPOINTS //
+// update
+app.put('/products/:id', function(req, res, next){
+
+})
+
+// DELETE ENDPOINTS //
+// delete products
+app.delete('/products/:id', function(req, res, next){
+
 })
 
 app.listen(9090, function(){
